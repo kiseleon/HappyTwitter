f4ea9ce491151978fb2cee23ef9b7fe3b60e1286
Kevin Johnson <bluechaos@gmail.com>
2016-03-31 09:34:32 -0600

    Fixed connection error so it continues to run.
    Database has more data now.

--- happy.py
+++ happy.py
@@ -22,24 +22,31 @@ keyword = 'joy,laughter,glad,blessed,ecstatic,excited,kindness,grace,sweetness,u
 
 r = api.request('statuses/filter', {'track': [keyword], 'language': 'en'})
 
-for item in r:
-    print(item['text'] if 'text' in item else item)
-    if 'text' in item:
-        statement = item['text']
-        split = statement.split(' ', 2)
-        username = split[0]
-        tweet = item['text']
-        rt_name = item['user']['screen_name']
-        tweet_id = item['id']
+while (True):
 
-        """This should split on retweets"""
-        if "RT" == split[0] and 'retweeted_status' in item:  # is didn't work here for whatever reason, so == it is
-            username = item['retweeted_status']['user']['screen_name']
-            conn.execute("INSERT INTO retweetUsers (username, rtusername, tweets, keywords, tweet_id, original_tweet_id) VALUES (?, ?, ?, ?, ?, ?)", [username, rt_name, tweet, keyword, tweet_id, item['retweeted_status']['id']])
-            conn.commit()
-        else:  # "@" is split[0][0]: # This only grabs tweets that start by referencing another person, whereas we want all non-retweets here
-            conn.execute("INSERT INTO parentUsers (username, tweets, keywords, tweet_id) VALUES (?, ?, ?, ?)", [rt_name, tweet, keyword, tweet_id])
-            conn.commit()
+    try:
+        for item in r:
+            print(item['text'] if 'text' in item else item)
+            if 'text' in item:
+                statement = item['text']
+                split = statement.split(' ', 2)
+                username = split[0]
+                tweet = item['text']
+                rt_name = item['user']['screen_name']
+                tweet_id = item['id']
+
+                """This should split on retweets"""
+                if "RT" == split[0] and 'retweeted_status' in item:  # is didn't work here for whatever reason, so == it is
+                    username = item['retweeted_status']['user']['screen_name']
+                    conn.execute("INSERT INTO retweetUsers (username, rtusername, tweets, keywords, tweet_id, original_tweet_id) VALUES (?, ?, ?, ?, ?, ?)", [username, rt_name, tweet, keyword, tweet_id, item['retweeted_status']['id']])
+                    conn.commit()
+                else:  # "@" is split[0][0]: # This only grabs tweets that start by referencing another person, whereas we want all non-retweets here
+                    conn.execute("INSERT INTO parentUsers (username, tweets, keywords, tweet_id) VALUES (?, ?, ?, ?)", [rt_name, tweet, keyword, tweet_id])
+                    conn.commit()
+    except:
+        conn = db_connect()
+        r = api.request('statuses/filter', {'track': [keyword], 'language': 'en'})
+
 
 print("records created successful")
 conn.close()

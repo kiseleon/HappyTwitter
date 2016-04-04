1c52665341652863a18a407b65469988d48bde0c
johndecker <johnddecker1@gmail.com>
2016-04-04 16:39:08 -0600

    This is grabing properly

--- TwitterRelations.py
+++ TwitterRelations.py
@@ -1,10 +1,18 @@
 class TwitterRelations:
     def __init__(self, numberTweets, userList):
         self.user_list = userList
-        self.num_tweets = numberTweets
+        self.num_tweets = user
 
     def getUser_Tweets(self):
        return self.num_tweets
 
     def getUser_List(self):
         return self.user_list
+
+    def setUser_List(self,listItem):
+        self.user_list = listItem
+        return
+
+    def setNum_Tweets(self, num_tweets):
+        self.num_tweets = num_tweets
+        return
--- relationFinder.py
+++ relationFinder.py
@@ -1,18 +1,40 @@
 from pythonDB import db_connect
-import TwitterAPI
+from TwitterAPI import TwitterAPI
 from TwitterRelations import TwitterRelations
+import secrets
 
 conn = db_connect()
 c = conn.cursor()
+CONSUMER_KEY = secrets.get_consumer_key()
+CONSUMER_SECRET = secrets.get_consumer_secret()
+ACCESS_TOKEN_KEY = secrets.get_access_token_key()
+ACCESS_TOKEN_SECRET = secrets.get_access_token_secret()
 
+api = TwitterAPI(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET, auth_type='oAuth1')
+
+diction_Users = {}
 
 def grabDB():
 
     c.execute('SELECT original_tweet_id FROM retweetUsers')
     contents = c.fetchall()
-
+    i = 0
     for row in contents:
-        print "rows ", row
+        try:
+            print "rows ", row
+            r = api.request('statuses/show/:%d' % row)
+
+            for item in r:
+                if(item[''])
+                reUsers = item['in_reply_to_status_id']
+                list = reUsers
+                i = diction_Users[item['user']['screen_name']]
+                diction_Users[item['user']['screen_name']] = i+1
+        except:
+            conn = db_connect()
+            r = api.request('statuses/show/:%d' % row)
+
 
 
 grabDB()
+print diction_Users
